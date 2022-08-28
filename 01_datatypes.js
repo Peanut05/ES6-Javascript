a666e86eaccf8b611181e6122cb8a0991db110e7
krushna Gavali <krushnagawali744@gmail.com>
2022-08-28 11:06:27 -0100

    Create 01_datatypes.js

---
+++ 01_datatypes.js
@@ -0,0 +1,15 @@
+// Numbers in js:=>>
+console.log (10 + 5);
+console.log(Math.PI);
+console.log(+10);
+console.log(typeof(10));
+console.log(typeof(11.3232));
+console.log(isNaN("string "))
+
+// Strings in js:=>>
+console.log("Hello world");                 //string
+console.log("whats going on?".length);      //Length of string
+console.log("Peanut".charAt(5))             //which charecter at the place of in function
+console.log("Hello" + " "+ "World" + "!");  //concatnating two strings
+console.log("i want that text un uppercase".toUpperCase()); //convert text into uppercase
+
